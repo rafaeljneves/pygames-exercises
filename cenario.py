f31ffd1c3045ad6cf6fb39b42b8acff5a9c5dea5
Rafael Neves <rafaeljneves@gmail.com>
2017-10-19 16:49:20 -0200

    COMMIT INICIAL

---
+++ cenario.py
@@ -0,0 +1,13 @@
+"""
+exercícios de cenario.py
+
+com pygames
+"""
+
+import pygame
+
+from pygame.locals import *
+
+screenDimension = (500,500)
+screen = pygame.display.set_mode(screenDimension,0,32)
+
